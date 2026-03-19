8ce789e2ec50a574180bfcfd9b2ee355b82c26c8
Kalam <mdkalam1905@gmail.com>
2026-03-19 16:54:18 +0530

    phishguard project

---
+++ backend/app.py
@@ -0,0 +1,66 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+
+#------CREATE REPLACEMENT-----
+replacements = {
+    "4": "a",
+    "0": "o",
+    "1": "l",
+    "3": "e",
+    "5": "s"
+}
+
+def normalize(text):
+    for key, value in replacements.items():
+        text = text.replace(key, value)
+    return text
+
+
+#------HOME-------
+@app.route("/")
+def home():
+    return "Backend is running"
+
+
+#-----URL----
+@app.route("/url", methods=["POST"])
+def get_url():
+    data = request.json
+    url = data.get("url")    
+    if not url:
+        return jsonify({"error":"No url provided"}), 400
+    url = url.lower()    
+    normalized_url = normalize(url)
+    risk = 0
+    if "http://" in url:
+        risk += 1
+    if "login" in url or "verify" in url:
+        risk += 1
+    if url.endswith(".xyz") or url.endswith(".ru"):
+        risk += 1
+    brands = ["amazon", "google", "paypal"]
+    keywords = ["login", "verify", "secure"]
+    # for brand in brands:   
+    #     if brand in url:
+    #         if any(word in url  for word in keywords):
+    #                 risk += 2
+    for brand in brands:
+        if brand in normalized_url:
+            if any(word in normalized_url for word in keywords):
+                risk += 2
+                break
+    if risk >= 2:
+        result = "Phishing"
+    elif risk == 1:
+        result = "Suspicious"
+    else:
+        result = "Safe"
+    return jsonify({"url":url,
+                    "risk": risk,
+                    "result": result})
+        
+if __name__ == "__main__":
+    app.run(debug=True)
+    
---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+function App() {
+  const [url, setUrl] = useState("");
+  const [result, setResult] = useState("");
+  const [risk, setRisk] = useState(0);
+  function checkUrl() {
+      fetch("http://127.0.0.1:5000/url",{
+      method: "POST",
+      headers: {"Content-Type":"application/json"},
+      body:JSON.stringify({url:url})  
+    })
+    .then(res => res.json())
+    .then(data => {
+        setResult(data.result);
+        setRisk(data.risk)
+    });
+  }
+  return (
+    <div>
+      <input type="text" value={url} placeholder="Enter your url" onChange={(e) => setUrl(e.target.value)} /> <br/>
+      <button onClick={checkUrl}>Check</button>
+
+      <h2>Result: {result}</h2>
+      <p>Risk Score: {risk}</p>
+    </div>
+  )
+}
+export default App;
